fba8073cd33fbf4a7fccdfed4da502843d56d779
Diego Wu <diegowu@Mac.lan>
2025-03-16 11:44:32 -0500

    release key

--- app/src/main/java/com/example/fetch_api_activity/MainActivity.kt
+++ app/src/main/java/com/example/fetch_api_activity/MainActivity.kt
@@ -33,7 +33,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
     @Composable
     fun ItemList() {
         val fetchRewardsViewModel: FetchRewardsViewModel = viewModel()
